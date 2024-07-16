f9dc788ec75083ebf597ab63b7171861b0425ad6
AndorkaDominik <dominik.andorka@gmail.com>
2024-07-16 12:16:41 +0200

    add: player names

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -4,15 +4,16 @@ import circle_icon from '../Assets/circle.png';
 import cross_icon from '../Assets/cross.png';
 
 let data = [
-        "", "", "",
-        "", "", "",
-        "", "", "",
-    ]
+    "", "", "",
+    "", "", "",
+    "", "", "",
+]
 
 const TicTacToe = () => {
-
     let [count, setCount] = useState(0);
     let [lock, setLock] = useState(false);
+    let [playerNames, setPlayerNames] = useState({ x: '', o: '' });
+    let [playersEntered, setPlayersEntered] = useState(false);
     let titleRef = useRef(null);
 
     let box1 = useRef(null);
@@ -32,98 +33,126 @@ const TicTacToe = () => {
     ]
 
     const toggle = (e, num) => {
-        if(lock){
+        if (lock) {
             return 0;
         }
-        if (count % 2 === 0){
-            e.target.innerHTML = `<img src='${cross_icon}'>`
-            data[num] = "x";
-            setCount(++count);
+        if (data[num] !== "") {
+            return 0;
         }
-        else{
-            e.target.innerHTML = `<img src='${circle_icon}'>`
+        if (count % 2 === 0) {
+            e.target.innerHTML = `<img src='${cross_icon}' alt='x' />`
+            data[num] = "x";
+            setCount(count + 1);
+        } else {
+            e.target.innerHTML = `<img src='${circle_icon}' alt='o' />`
             data[num] = "o";
-            setCount(++count);
+            setCount(count + 1);
         }
         checkWin();
     }
 
     const checkWin = () => {
-        if(data[0] === data[1] && data[1]===data[2] && data[2] !== ""){
+        if (data[0] === data[1] && data[1] === data[2] && data[2] !== "") {
             won(data[2]);
-        }
-        else if(data[3] === data[4] && data[4]===data[5] && data[5] !== ""){
+        } else if (data[3] === data[4] && data[4] === data[5] && data[5] !== "") {
             won(data[5]);
-        }
-        else if(data[6] === data[7] && data[7]===data[8] && data[8] !== ""){
+        } else if (data[6] === data[7] && data[7] === data[8] && data[8] !== "") {
             won(data[8]);
-        }
-        else if(data[0] === data[3] && data[3]===data[6] && data[6] !== ""){
+        } else if (data[0] === data[3] && data[3] === data[6] && data[6] !== "") {
             won(data[6]);
-        }
-        else if(data[1] === data[4] && data[4]===data[7] && data[7] !== ""){
+        } else if (data[1] === data[4] && data[4] === data[7] && data[7] !== "") {
             won(data[7]);
-        }
-        else if(data[2] === data[5] && data[5]===data[8] && data[8] !== ""){
+        } else if (data[2] === data[5] && data[5] === data[8] && data[8] !== "") {
             won(data[8]);
-        }
-        else if(data[0] === data[4] && data[4]===data[8] && data[8] !== ""){
+        } else if (data[0] === data[4] && data[4] === data[8] && data[8] !== "") {
             won(data[8]);
-        }
-        else if(data[0] === data[1] && data[1]===data[2] && data[2] !== ""){
-            won(data[2]);
-        }
-        else if(data[2] === data[4] && data[4]===data[6] && data[6] !== ""){
+        } else if (data[2] === data[4] && data[4] === data[6] && data[6] !== "") {
             won(data[6]);
         }
     }
 
     const won = (winner) => {
         setLock(true);
-        if(winner==="x"){
-            titleRef.current.innerHTML = `Congratulations: <img src=${cross_icon} /> Wins`
-        }
-        else{
-            titleRef.current.innerHTML = `Congratulations: <img src=${circle_icon} /> Wins`
+        if (winner === "x") {
+            titleRef.current.innerHTML = playerNames.x ? 
+                `Congratulations: ${playerNames.x} Wins` : 
+                `Congratulations: <img src=${cross_icon} alt='x' /> Wins`;
+        } else {
+            titleRef.current.innerHTML = playerNames.o ? 
+                `Congratulations: ${playerNames.o} Wins` : 
+                `Congratulations: <img src=${circle_icon} alt='o' /> Wins`;
         }
     }
 
     const reset = () => {
         setLock(false);
         data = [
-        "", "", "",
-        "", "", "",
-        "", "", "",
+            "", "", "",
+            "", "", "",
+            "", "", "",
         ];
-        titleRef.current.innerHTML = `TicTacToe Game in<span>React</span>`
-        box_array.map((e) => {
+        titleRef.current.innerHTML = `TicTacToe Game in<span>React</span>`;
+        box_array.forEach((e) => {
             e.current.innerHTML = "";
-        })
+        });
+        setCount(0);
     }
 
-  return (
-    <div className='container'>
-        <h1 className="title" ref={titleRef}>TicTacToe Game in<span>React</span></h1>
-        <div className="board">
-            <div className="row1">
-                <div className="boxes" ref={box1} onClick={(e) => {toggle(e,0)}}></div>
-                <div className="boxes" ref={box2} onClick={(e) => {toggle(e,1)}}></div>
-                <div className="boxes" ref={box3} onClick={(e) => {toggle(e,2)}}></div>
+    const handleNameSubmit = (e) => {
+        e.preventDefault();
+        const playerXName = e.target.playerX.value || 'Player X';
+        const playerOName = e.target.playerO.value || 'Player O';
+        setPlayerNames({ x: playerXName, o: playerOName });
+        setPlayersEntered(true);
+    }
+
+    if (!playersEntered) {
+        return (
+            <div className="name-entry-container">
+                <h1>Enter Player Names</h1>
+                <form onSubmit={handleNameSubmit}>
+                    <label>
+                        Player X:
+                        <input type="text" name="playerX" placeholder="Player X" required />
+                    </label>
+                    <label>
+                        Player O:
+                        <input type="text" name="playerO" placeholder="Player O" required />
+                    </label>
+                    <div className="button-container">
+                        <button type="submit">Start Game</button>
+                        <button type="button" onClick={() => setPlayersEntered(true)}>Skip</button>
+                    </div>
+                </form>
             </div>
-            <div className="row2">
-                <div className="boxes" ref={box4} onClick={(e) => {toggle(e,3)}}></div>
-                <div className="boxes" ref={box5} onClick={(e) => {toggle(e,4)}}></div>
-                <div className="boxes" ref={box6} onClick={(e) => {toggle(e,5)}}></div>
+        );
+    }
+
+    return (
+        <div className='container'>
+            <h1 className="title" ref={titleRef}>TicTacToe Game in<span>React</span></h1>
+            <div className="board">
+                <div className="row1">
+                    <div className="boxes" ref={box1} onClick={(e) => { toggle(e, 0) }}></div>
+                    <div className="boxes" ref={box2} onClick={(e) => { toggle(e, 1) }}></div>
+                    <div className="boxes" ref={box3} onClick={(e) => { toggle(e, 2) }}></div>
+                </div>
+                <div className="row2">
+                    <div className="boxes" ref={box4} onClick={(e) => { toggle(e, 3) }}></div>
+                    <div className="boxes" ref={box5} onClick={(e) => { toggle(e, 4) }}></div>
+                    <div className="boxes" ref={box6} onClick={(e) => { toggle(e, 5) }}></div>
+                </div>
+                <div className="row3">
+                    <div className="boxes" ref={box7} onClick={(e) => { toggle(e, 6) }}></div>
+                    <div className="boxes" ref={box8} onClick={(e) => { toggle(e, 7) }}></div>
+                    <div className="boxes" ref={box9} onClick={(e) => { toggle(e, 8) }}></div>
+                </div>
             </div>
-            <div className="row3">
-                <div className="boxes" ref={box7} onClick={(e) => {toggle(e,6)}}></div>
-                <div className="boxes" ref={box8} onClick={(e) => {toggle(e,7)}}></div>
-                <div className="boxes" ref={box9} onClick={(e) => {toggle(e,8)}}></div>
+            <div className="button-container">
+            <button className="reset" onClick={() => { reset() }}>Reset</button>
             </div>
         </div>
-        <button className="reset" onClick={() => {reset()}}>Reset</button>
-    </div>
-  )
+    )
 }
 
-export default TicTacToe
+export default TicTacToe;
